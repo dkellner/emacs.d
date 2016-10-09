915b3a806faf1d73c6d924a7737033b56b31d892
Dominik Kellner <dkellner@dkellner.de>
2016-10-09 23:16:09 +0200

    Add `dkellner/smarter-move-beginning-of-line'

--- conf/dkellner-misc.el
+++ conf/dkellner-misc.el
@@ -39,4 +39,32 @@
   (local-set-key (kbd "C-c g") 'recompile))
 (add-hook 'compilation-mode-hook 'dkellner/bind-recompile)
 
+;; Make C-a and <home> toggle between the first non-whitespace character and
+;; the actual beginning of the line.
+;; See http://emacsredux.com/blog/2013/05/22/smarter-navigation-to-the-beginning-of-a-line/
+(defun dkellner/smarter-move-beginning-of-line (arg)
+  "Move point back to indentation of beginning of line.
+
+Move point to the first non-whitespace character on this line.
+If point is already there, move to the beginning of the line.
+Effectively toggle between the first non-whitespace character and
+the beginning of the line.
+
+If ARG is not nil or 1, move forward ARG - 1 lines first.  If
+point reaches the beginning or end of the buffer, stop there."
+  (interactive "^p")
+  (setq arg (or arg 1))
+
+  ;; Move lines first
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
+
+  (let ((orig-point (point)))
+    (back-to-indentation)
+    (when (= orig-point (point))
+      (move-beginning-of-line 1))))
+(global-set-key [remap move-beginning-of-line]
+                'dkellner/smarter-move-beginning-of-line)
+
 (provide 'dkellner-misc)

d2d3d58ad4b2f005f0f79db91c3af2478ef879a5
Dominik Kellner <dkellner@dkellner.de>
2016-10-09 22:25:16 +0200

    Add more Helm keybindings

--- conf/dkellner-helm.el
+++ conf/dkellner-helm.el
@@ -5,13 +5,17 @@
 (use-package helm
   ;; I want to use the more powerful "equivalents" most of the time, so I bind
   ;; them to the default Emacs shortcuts.
-  :bind (("M-x" . helm-M-x)
-         ("\C-x\C-f" . helm-find-files)
-         ("\C-xb" . helm-mini)
-         ("\C-ha" . helm-apropos))
+  :bind (("C-c g" . helm-grep-do-git-grep)
+         ("C-h a" . helm-apropos)
+         ("C-x C-f" . helm-find-files)
+         ("C-x b" . helm-mini)
+         ("M-x" . helm-M-x)
+         ("M-y" . helm-show-kill-ring))
   :config
   (require 'helm-config)
   (helm-mode 1)
+  (use-package helm-swoop
+    :bind ("C-c s" . helm-swoop))
   (use-package helm-descbinds
     :config
     (helm-descbinds-mode))

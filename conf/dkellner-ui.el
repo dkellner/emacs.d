482fb65eb41dee5bf3cae47b58c8dc7e5add719d
Dominik Kellner <dkellner@dkellner.de>
2016-10-26 13:13:19 +0200

    Set font and transparency correctly (not just for the active frame)

--- conf/dkellner-ui.el
+++ conf/dkellner-ui.el
@@ -16,9 +16,9 @@
 (scroll-bar-mode -1)
 (show-paren-mode t)
 
-(set-face-font 'default "Hack 12")
+(add-to-list 'default-frame-alist '(font . "Hack 12"))
 
 ;; Transparency
-(set-frame-parameter (selected-frame) 'alpha 93)
+(add-to-list 'default-frame-alist '(alpha . 93))
 
 (provide 'dkellner-ui)

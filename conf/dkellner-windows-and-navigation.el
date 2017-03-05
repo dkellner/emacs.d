e2929d2e810566225e09860f297f4f38600248d4
Dominik Kellner <dkellner@dkellner.de>
2017-03-05 19:05:57 +0700

    Use a prefix-map instead of a hydra for window switching

--- conf/dkellner-windows-and-navigation.el
+++ conf/dkellner-windows-and-navigation.el
@@ -3,13 +3,13 @@
 ;; Window navigation with windmove. I use a pretty neat trick here: binding to
 ;; a character I rarely type allows me to use comfortable keystroke
 ;; combinations, but by typing it twice I can still insert the character.
-(defhydra dkellner/windmove (:exit t)
-  ("<left>" windmove-left "left")
-  ("<right>" windmove-right "right")
-  ("<up>" windmove-up "up")
-  ("<down>" windmove-down "down")
-  ("¿" self-insert-command "insert ¿"))
-(bind-key* "¿" #'dkellner/windmove/body)
+(bind-keys :prefix-map windmove-map
+           :prefix "¿"
+           ("<left>" . windmove-left)
+           ("<right>" . windmove-right)
+           ("<up>" . windmove-up)
+           ("<down>" . windmove-down)
+           ("¿" . self-insert-command))
 
 ;; Enable winner-mode and enhance its functionality by making the keybindings
 ;; "sticky" - i.e. let you press C-c <left> to undo once, and then just <left>

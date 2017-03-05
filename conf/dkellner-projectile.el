27d247d972367815beb513b1d2717c54584ca28b
Dominik Kellner <dkellner@dkellner.de>
2017-03-05 19:08:11 +0700

    Add `ibuffer-projectile`

--- conf/dkellner-projectile.el
+++ conf/dkellner-projectile.el
@@ -8,4 +8,12 @@
   (setq projectile-completion-system 'ivy)
   :diminish projectile-mode)
 
+(use-package ibuffer-projectile
+  :config
+  (add-hook 'ibuffer-hook
+            (lambda ()
+              (ibuffer-projectile-set-filter-groups)
+              (unless (eq ibuffer-sorting-mode 'alphabetic)
+                (ibuffer-do-sort-by-alphabetic)))))
+
 (provide 'dkellner-projectile)

753bc73e8c49c6efa0d12c50fa56d5423aaf3a9c
Dominik Kellner <dkellner@dkellner.de>
2017-03-05 19:09:09 +0700

    Customize the modeline without `powerline`

--- conf/dkellner-ui.el
+++ conf/dkellner-ui.el
@@ -2,11 +2,13 @@
 
 (use-package material-theme
   :config
-  (load-theme 'material t))
-
-(use-package powerline
-  :config
-  (powerline-default-theme))
+  (load-theme 'material t)
+  (custom-theme-set-faces
+   'material
+   '(mode-line ((t (:background "#0d47a1"))))
+   '(mode-line-buffer-id ((t (:foreground "#fff59d"))))
+   '(mode-line-inactive ((t (:background "#3a3a3a"))))
+   '(org-mode-line-clock ((t (:background nil))))))
 
 (use-package bar-cursor
   :config
@@ -27,5 +29,26 @@
 
 ;; Transparency
 (add-to-list 'default-frame-alist '(alpha . 93))
+
+;; Mode-Line, mostly inspired by this post:
+;; http://www.holgerschurig.de/en/emacs-tayloring-the-built-in-mode-line/9
+(column-number-mode 1)
+(setq mode-line-position
+      '((line-number-mode ("%l" (column-number-mode ":%c")))))
+(setq-default mode-line-format
+              '("%e"
+                mode-line-front-space
+                mode-line-mule-info
+                mode-line-client
+                mode-line-modified
+                " "
+                mode-line-buffer-identification
+                " "
+                mode-line-position
+                (flycheck-mode flycheck-mode-line)
+                " "
+                mode-line-modes
+                mode-line-misc-info
+                mode-line-end-spaces))
 
 (provide 'dkellner-ui)

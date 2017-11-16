8204e5fffc5933f50279f43a3f148b24696e8bc9
Dominik Kellner <dkellner@dkellner.de>
2017-11-16 18:24:18 +0100

    Add `web-mode`

---
+++ conf/dkellner-web-mode.el
@@ -0,0 +1,32 @@
+;; dkellner-web-mode.el --- Web-development
+
+(use-package web-mode
+  :config
+  (add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+  (setq-default web-mode-markup-indent-offset 2)
+  (setq-default web-mode-css-indent-offset 2)
+  (setq-default web-mode-code-indent-offset 2)
+
+  (use-package company-php
+    :config
+    (add-hook 'web-mode-before-auto-complete-hooks
+              '(lambda ()
+                 (let ((web-mode-cur-language
+                        (web-mode-language-at-pos)))
+                   (if (string= web-mode-cur-language "php")
+                       (progn
+                         (add-to-list 'company-backends
+                                      '(company-ac-php-backend
+                                        company-dabbrev))
+                         (flycheck-mode))
+                     (yas-deactivate-extra-mode 'php-mode)))))))
+
+(provide 'dkellner-web-mode)

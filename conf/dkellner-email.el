40277aada0e1e8b79161d729ead0acd5fbe803eb
Dominik Kellner <dkellner@dkellner.de>
2018-08-11 17:31:33 +0700

    Fix mutt integration for Emacs 26.1
    The behaviour of `fill-paragraph` in `message-mode` was changed, so I've
    switched to using `mail-mode` for mutt.

--- conf/dkellner-email.el
+++ conf/dkellner-email.el
@@ -1,5 +1,7 @@
 ;; dkellner-email.el --- improve integration with mutt
 
-(add-to-list 'auto-mode-alist '("/tmp/mutt-" . message-mode))
+(add-to-list 'auto-mode-alist '("/tmp/mutt-" . mail-mode))
+
+(add-hook 'mail-mode-hook #'turn-on-auto-fill)
 
 (provide 'dkellner-email)

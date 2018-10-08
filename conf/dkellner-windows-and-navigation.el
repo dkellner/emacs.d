9c15f0cea61c2d6abd971cfac7b92912bb34ec3e
Dominik Kellner <dkellner@dkellner.de>
2018-10-08 14:50:04 +0700

    Remove `dkellner/bring-buffer' - I don't really use it anymore

--- conf/dkellner-windows-and-navigation.el
+++ conf/dkellner-windows-and-navigation.el
@@ -63,15 +63,4 @@
   ("<right>" winner-redo))
 (bind-key* "C-c <left>" #'dkellner/winner-undo/body)
 
-(defun dkellner/bring-buffer (buffer-or-name)
-  "Brings a buffer to the current window. If the buffer is currently displayed
-in another window, it will be swapped with the current buffer."
-  (interactive "bBuffer: ")
-  (let ((current-window (get-buffer-window (current-buffer)))
-        (other-window (get-buffer-window buffer-or-name)))
-    (when other-window (set-window-buffer other-window (current-buffer)))
-    (switch-to-buffer buffer-or-name)))
-
-(bind-key "C-x b" #'dkellner/bring-buffer)
-
 (provide 'dkellner-windows-and-navigation)

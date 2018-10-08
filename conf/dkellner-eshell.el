e213d5a49bad862408d8823c0636eadf078c5661
Dominik Kellner <dkellner@dkellner.de>
2018-10-08 12:48:54 +0700

    Optimize Eshell setup
    Since switching to EXWM I'm trying again to use Eshell as my main shell. So far
    it works quite well.

--- conf/dkellner-eshell.el
+++ conf/dkellner-eshell.el
@@ -1,11 +1,38 @@
-;; dkellner-eshell.el --- EShell configuration
+;; dkellner-eshell.el --- Eshell configuration
 
 (add-hook 'eshell-first-time-mode-hook #'dkellner/setup-eshell)
+(add-hook 'eshell-mode-hook #'dkellner/add-eshell-keys)
+
+(setq eshell-history-size 10000)
 
 (defun dkellner/setup-eshell ()
-  (add-to-list 'eshell-visual-commands "mysql")
-  (add-to-list 'eshell-visual-commands "mplayer")
-  (add-to-list 'eshell-visual-commands "mutt-dkellner")
-  (add-to-list 'eshell-visual-commands "mutt-puzzleandplay"))
+  (setq eshell-visual-commands
+        '("ssh" "mutt-puzzleandplay" "mutt-dkellner" "mplayer" "mysql" "vi"
+          "screen" "top" "less" "more"))
+  (setq eshell-visual-subcommands
+        '(("nixops" "ssh")))
+  (setq eshell-where-to-jump 'begin
+        eshell-review-quick-commands nil
+        eshell-smart-space-goes-to-end t))
+
+(defun dkellner/add-eshell-keys ()
+  (define-key eshell-mode-map (kbd "C-c C-l") #'counsel-esh-history)
+  (define-key eshell-mode-map (kbd "C-x k") #'kill-buffer-and-window)
+  (define-key eshell-mode-map (kbd "<up>") #'previous-line)
+  (define-key eshell-mode-map (kbd "<down>") #'next-line))
+
+(defun dkellner/new-eshell ()
+  "Open a new Eshell.
+
+The shell is opened in the directory associated with the current
+buffer's file. The eshell buffer is renamed to match that
+directory."
+  (interactive)
+  (let* ((dir (if (buffer-file-name)
+                  (file-name-directory (buffer-file-name))
+                default-directory))
+         (name (abbreviate-file-name dir)))
+    (eshell t)
+    (rename-buffer (concat "*eshell:" name "*"))))
 
 (provide 'dkellner-eshell)

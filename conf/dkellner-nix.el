f96348da1be2db60a359ce901be7a59d353cc093
Dominik Kellner <dkellner@dkellner.de>
2018-10-10 09:15:17 +0700

    Remove nix-sandbox
    It throws errors now and then and I don't have time now to investigate. I don't
    really use it, anyway.

--- conf/dkellner-nix.el
+++ conf/dkellner-nix.el
@@ -2,13 +2,4 @@
 
 (use-package nix-mode)
 
-(use-package nix-sandbox
-  :config
-  (use-package company-nixos-options)
-  (add-to-list 'company-backends 'company-nixos-options)
-  (setq
-   flycheck-command-wrapper-function (lambda (command) (apply 'nix-shell-command (nix-current-sandbox) command))
-   flycheck-executable-find (lambda (cmd) (nix-executable-find (nix-current-sandbox) cmd)))
-  (setq haskell-process-wrapper-function (lambda (args) (apply 'nix-shell-command (nix-current-sandbox) args))))
-
 (provide 'dkellner-nix)

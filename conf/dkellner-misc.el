a2a9f420390d96a5047b20763eff330c7afc3fae
Dominik Kellner <dkellner@dkellner.de>
2018-10-23 08:34:33 +0700

    Increase size of kill ring

--- conf/dkellner-misc.el
+++ conf/dkellner-misc.el
@@ -2,6 +2,7 @@
 
 (setq default-directory "~/")
 (setq make-backup-files nil)
+(setq kill-ring-max 1000)
 (setq-default require-final-newline t)
 (setq-default indent-tabs-mode nil)
 (setq-default fill-column 79)

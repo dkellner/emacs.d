0b1a5cd4832b952a3cb4b540da69a1e087751139
Dominik Kellner <dkellner@dkellner.de>
2018-10-27 13:45:16 +0700

    Use `crux-move-beginning-of-line' instead of "own" version

--- conf/dkellner-misc.el
+++ conf/dkellner-misc.el
@@ -69,33 +69,6 @@
   (local-set-key (kbd "C-c g") 'recompile))
 (add-hook 'compilation-mode-hook 'dkellner/bind-recompile)
 
-;; Make C-a and <home> toggle between the first non-whitespace character and
-;; the actual beginning of the line.
-;; See http://emacsredux.com/blog/2013/05/22/smarter-navigation-to-the-beginning-of-a-line/
-(defun dkellner/smarter-move-beginning-of-line (arg)
-  "Move point back to indentation of beginning of line.
-
-Move point to the first non-whitespace character on this line.
-If point is already there, move to the beginning of the line.
-Effectively toggle between the first non-whitespace character and
-the beginning of the line.
-
-If ARG is not nil or 1, move forward ARG - 1 lines first.  If
-point reaches the beginning or end of the buffer, stop there."
-  (interactive "^p")
-  (setq arg (or arg 1))
-
-  ;; Move lines first
-  (when (/= arg 1)
-    (let ((line-move-visual nil))
-      (forward-line (1- arg))))
-
-  (let ((orig-point (point)))
-    (back-to-indentation)
-    (when (= orig-point (point))
-      (move-beginning-of-line 1))))
-(bind-key "<home>" 'dkellner/smarter-move-beginning-of-line)
-
 (use-package markdown-mode)
 (use-package highlight-indentation)
 (use-package yaml-mode
@@ -125,7 +98,8 @@ point reaches the beginning or end of the buffer, stop there."
 (setq shell-file-name "/bin/sh")
 
 (use-package crux
-  :bind (("C-c e" . crux-eval-and-replace)
+  :bind (([remap move-beginning-of-line] . crux-move-beginning-of-line)
+         ("C-c e" . crux-eval-and-replace)
          ("C-c o" . crux-open-with)
          ("C-c r" . crux-rename-file-and-buffer)
          ("C-c u" . crux-view-url)

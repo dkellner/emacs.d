b544e82a4d645e4fa73c8a31ce3147823e05db2c
Dominik Kellner <dkellner@dkellner.de>
2018-12-20 15:54:11 +0700

    Use `brittany` on save in `haskell-mode`

--- conf/dkellner-haskell.el
+++ conf/dkellner-haskell.el
@@ -1,5 +1,10 @@
 ;; dkellner-haskell.el --- Haskell-specific configuration
 
+(use-package haskell-mode
+  :config
+  (setq haskell-stylish-on-save t
+        haskell-mode-stylish-haskell-path "brittany"))
+
 ;; See http://commercialhaskell.github.io/intero/
 (use-package intero
   :config

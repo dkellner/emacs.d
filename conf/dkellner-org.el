06c1f659e1f3d4284b98dea2f7c2d574c8499753
Dominik Kellner <dkellner@dkellner.de>
2019-09-27 15:29:45 +0200

    Make org-drill work with org-mode 9.2

--- conf/dkellner-org.el
+++ conf/dkellner-org.el
@@ -113,4 +113,28 @@
                  :tag "@fiedlbuehl")))
   (org-super-agenda-mode 1))
 
+;; Make org-drill work with org-mode 9.2, see:
+;; https://bitbucket.org/eeeickythump/org-drill/issues/62/org-drill-doesnt-work-with-org-mode-92
+(defun org-drill-hide-subheadings-if (test)
+  "TEST is a function taking no arguments. TEST will be called for each
+of the immediate subheadings of the current drill item, with the point
+on the relevant subheading. TEST should return nil if the subheading is
+to be revealed, non-nil if it is to be hidden.
+Returns a list containing the position of each immediate subheading of
+the current topic."
+  (let ((drill-entry-level (org-current-level))
+        (drill-sections nil))
+    (org-show-subtree)
+    (save-excursion
+      (org-map-entries
+       (lambda ()
+         (when (and (not (org-invisible-p))
+                    (> (org-current-level) drill-entry-level))
+           (when (or (/= (org-current-level) (1+ drill-entry-level))
+                        (funcall test))
+             (hide-subtree))
+           (push (point) drill-sections)))
+       nil 'tree))
+    (reverse drill-sections)))
+
 (provide 'dkellner-org)
